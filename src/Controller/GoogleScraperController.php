72e36c4ff23df84913f48c9312e7451172679b2f
Matej Križanec <matej.krizanec@undabot.com>
2022-11-17 16:36:37 +0100

    New Methods Scraper
    - Detail
    - Top Selling
    - Top Selling Gross

--- src/Controller/GoogleScraperController.php
+++ src/Controller/GoogleScraperController.php
@@ -48,7 +48,11 @@ class GoogleScraperController extends AbstractController
     #[Route('/detail/{id}', name: 'detail')]
     public function detail($id, Request $request) : JsonResponse
     {
-        return $this->json(null);
+        $service = new GPlayApps('hr_HR', 'hr');
+
+        return $this->json(
+            $service->getAppInfo($id)
+        );
     }
 
     #[Route('/categories', name: 'categories')]
@@ -58,6 +62,40 @@ class GoogleScraperController extends AbstractController
 
         return $this->json(
             $service->getCategories()
+        );
+    }
+
+    #[Route('/top-selling/{type}/{category}', name: 'top_selling')]
+    public function topSelling($type, $category, Request $request) : JsonResponse
+    {
+        (int) $limit = $request->get('limit', 10);
+
+        $service = new GPlayApps('hr_HR', 'hr');
+
+        if ($type == 'free') {
+            return $this->json(
+                $service->getTopSellingFreeApps(/*CategoryEnum::BEAUTY()*/ $category, $limit)
+            );
+        }
+
+        if ($type == 'paid') {
+            return $this->json(
+                $service->getTopSellingPaidApps(/*CategoryEnum::BEAUTY()*/ $category, $limit)
+            );
+        }
+
+        return $this->json(null);
+    }
+
+    #[Route('/top-grossing/{category}', name: 'top_grossing')]
+    public function topGrossing($category, Request $request) : JsonResponse
+    {
+        (int) $limit = $request->get('limit', 10);
+
+        $service = new GPlayApps('hr_HR', 'hr');
+
+        return $this->json(
+            $service->getTopGrossingApps($category, $limit)
         );
     }
 }
