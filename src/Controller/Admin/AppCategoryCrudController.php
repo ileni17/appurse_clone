507072ebc566e417bbfc2c7b05550551465ebbc3
Matej Križanec <matej.krizanec@undabot.com>
2022-11-21 13:47:04 +0100

    EasyAdmin CRUD Configure Fields

--- src/Controller/Admin/AppCategoryCrudController.php
+++ src/Controller/Admin/AppCategoryCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\AppCategory;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class AppCategoryCrudController extends AbstractCrudController
@@ -10,6 +11,14 @@ class AppCategoryCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return AppCategory::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('App Category')
+            ->setEntityLabelInPlural('App Categories')
+            ->setDefaultSort(['id' => 'DESC']);
     }
 
     /*
--- src/Controller/Admin/AppCrudController.php
+++ src/Controller/Admin/AppCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\App;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
 class AppCrudController extends AbstractCrudController
 {
@@ -12,14 +19,24 @@ class AppCrudController extends AbstractCrudController
         return App::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('App')
+            ->setEntityLabelInPlural('Apps')
+            ->setDefaultSort(['id' => 'DESC']);
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->hideOnForm();
+        yield TextField::new('identificator', 'Identificator');
+        yield AssociationField::new('category', 'Category');
+        yield TextField::new('name', 'Name');
+        yield TextEditorField::new('description', 'Description');
+        yield NumberField::new('score', 'Score');
+        yield UrlField::new('app_url', 'App');
+        yield UrlField::new('icon_url', 'Icon');
     }
-    */
 }
